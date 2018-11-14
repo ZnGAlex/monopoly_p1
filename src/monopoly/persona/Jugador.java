dc062e102555fa2032a70f4019d27869ec11b2da
adriankou <44620324+adriankou@users.noreply.github.com>
2018-11-14 13:52:06 +0100

    cambios varios

--- src/monopoly/persona/Jugador.java
+++ src/monopoly/persona/Jugador.java
@@ -199,7 +199,7 @@ public class Jugador {
                             System.out.println("Opcion incorrecta.");
                             break;
                     }
-                } while (opcion.equals("bancarrota") || opcion.equals("hipotecarse"))
+                } while (opcion.equals("bancarrota") || opcion.equals("hipotecarse"));
             }
         }
         else if (this.inCarcel && this.turnosEnCarcel != 3) {
@@ -213,6 +213,7 @@ public class Jugador {
         } else {
             if (dados.dadosIguales()) {
                 System.out.println("Dados dobles.");
+                this.dadosTirados = false;
                 this.setDadosDobles(this.getDadosDobles() + 1);
             }
             if (this.getDadosDobles() == 3) {
@@ -245,11 +246,25 @@ public class Jugador {
         
     }
     
-    public void pagarImpuesto(int impuesto){
-        if(impuesto > this.fortuna){
+    public void pagarImpuesto(int impuesto,Tablero tablero){
+        while(impuesto > this.fortuna && !bancarrota){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("No tienes suficiente dinero. ¿Quieres hipotecar o declararte en bancarrota?: ");
+            String opcion = scanner.nextLine();
+            
+            switch(opcion){
+                case "hipotecarse":
+                    this.hipotecar();
+                    break;
+                case "bancarrota":
+                    this.declararBancarrota(tablero.getCasillas().get(0).get(0).getPropietario());
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta, las opciones son 'hipotecarse' y 'bancarrota'");
+            }
             
         }
-        else{
+        if(!bancarrota){
             this.fortuna -= impuesto;
             Valor.DINERO_PARKING += impuesto;
         }
@@ -263,10 +278,26 @@ public class Jugador {
     public void pagarAlquiler(){
         if(!this.avatar.getCasilla().getPropietario().equals(this)){
             if(!this.avatar.getCasilla().getPropietario().getNombre().equals("banca")){
-                if(this.avatar.getCasilla().getAlquiler()>this.fortuna){
+                    while(this.avatar.getCasilla().getAlquiler() > this.fortuna && !bancarrota){
+                        Scanner scanner = new Scanner(System.in);
+                        System.out.println("No tienes suficiente dinero. ¿Quieres hipotecar o declararte en bancarrota?: ");
+                        String opcion = scanner.nextLine();
+
+                        switch(opcion){
+                            case "hipotecarse":
+                                this.hipotecar();
+                                break;
+                            case "bancarrota":
+                                this.declararBancarrota(this.avatar.getCasilla().getPropietario());
+                                break;
+                            default:
+                                System.out.println("Opcion incorrecta, las opciones son 'hipotecarse' y 'bancarrota'");
+                        }
+
+                    }
                     System.out.println("No tienes suficiente dinero, quieres hipotecar o declararte en bancarrota");
-                }
-                else{
+                
+                if(!bancarrota){
                     this.fortuna-=this.avatar.getCasilla().getAlquiler();
                     this.avatar.getCasilla().getPropietario().cobrarAlquiler(this.avatar.getCasilla().getAlquiler());
                 }
